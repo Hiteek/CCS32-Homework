65195cdfb4c7c426f87732ec3e71f1d2e009c519
JDomCamZ <juansehermanito@gmail.com>
2022-07-20 19:49:15 -0500

    Finishing SquarePegAdapter.

--- src/Adapter/SquarePegAdapter.java
+++ src/Adapter/SquarePegAdapter.java
@@ -1,15 +1,14 @@
 public class SquarePegAdapter extends RoundPeg{
-    private SquarePeg peg;
+    private final SquarePeg peg;
 
     public SquarePegAdapter(SquarePeg peg) {
         this.peg = peg;
     }
 
     @Override
-    public double getRadius() {
-        double result;
-        // Calculate a minimum circle radius, which can fit this peg.
-        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+    public int getRadius() {
+        int result;
+        result = (int) Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2);
         return result;
     }
 
